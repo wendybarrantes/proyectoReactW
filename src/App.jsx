2abe4e84a698192735b098180c00b9f69bd6f1ab
wendybarrantes <wenbarrantes06@gmail.com>
2024-06-17 11:53:05 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <nav>
+      <a>Inicio</a>
+      <a>Iniciar Sesión</a>
+   </nav>
+   </>
+  )
+}
+
